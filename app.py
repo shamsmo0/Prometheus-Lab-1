a0cae9be038cc8ccc0a8198a0114b6415906bb64
Shams Mohamd <108243138+shamsmo0@users.noreply.github.com>
2026-06-20 17:41:47 +0300

    Initialize Flask app with Prometheus metrics

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+
+app = Flask(__name__)
+
+# Metrics
+REQUEST_COUNT = Counter('app_requests_total', 'Total App Requests')
+
+@app.route('/')
+def home():
+    REQUEST_COUNT.inc()
+    return "Hello from Python App!"
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
